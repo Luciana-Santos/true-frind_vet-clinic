0e8bf2dab5492d178cff207364298c3de3692db5
Luciana Santos <luciydss@hotmail.com>
2022-01-01 17:32:35 -0300

    estilos header mobile first

--- app/js/script.js
+++
@@ -1,8 +0,0 @@
-// const btnMobile = document.getElementById('btn-mobile');
-
-// function toggleMenu() {
-// 	const nav = document.querySelector('.nav');
-// 	nav.classList.toggle('active')
-// };
-
-// btnMobile.addEventListener('click', toggleMenu);
