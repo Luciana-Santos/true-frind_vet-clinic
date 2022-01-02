5753e94d05cc6b377e0706608c25f683f8b6db80
Luciana Santos <luciydss@hotmail.com>
2022-01-01 23:09:01 -0300

    header mobile finalizado

---
+++ app/js/script.js
@@ -0,0 +1,29 @@
+const openMenu = document.getElementById('open-menu');
+const closeMenu = document.getElementById('close-menu');
+const body = document.querySelector('body');
+const menu = document.getElementById('menu');
+const fadeElems = document.querySelectorAll('.has-fade');
+
+
+function abrirMenu() {
+  body.classList.remove('noscroll');
+    menu.classList.add('ativo');
+    fadeElems.forEach(function(element) {
+      element.classList.remove('fade-out');
+      element.classList.add('fade-in');
+    })
+}
+
+function fecharMenu() {
+  body.classList.remove('noscroll');
+    menu.classList.remove('ativo');
+    fadeElems.forEach(function(element) {
+      element.classList.remove('fade-in');
+      element.classList.add('fade-out');
+    });
+}
+
+
+openMenu.addEventListener('click', abrirMenu);
+closeMenu.addEventListener('click', fecharMenu);
+
